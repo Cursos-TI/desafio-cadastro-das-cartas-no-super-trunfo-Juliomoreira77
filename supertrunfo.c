8591515df6c77ccd945c3b959b53f9cb4f454d7c
Juliomoreira77 <ducajulio77@gmail.com>
2025-04-26 20:14:49 +0000

    Criei uma nova tela

--- supertrunfo.c
+++ supertrunfo.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 int main(){
 
-    
+    char estado = " Bahia";
+    char código [20];
+    char cidade [30];
+    int população;
+    float área;
+    float PIB;
+    int turismo;
+
+    printf(" Digite o estado ");
+    scanf(" %s estado ");
 }
